07c9694301b98132c4f5b85d74efd39d5d5998d0
Dingyx0100 <dingyx_christina@outlook.com>
2021-12-04 17:47:45 +0800

    add css and bg picture for login page and made adjustment

--- source-code/safe/safe/login.py
+++ source-code/safe/safe/login.py
@@ -1,37 +1,35 @@
-import hashlib
 from DBUser.models import DBUser
 from django.shortcuts import render
 from django.shortcuts import HttpResponseRedirect
 
 
-def setPassword(password):
-    """
-    加密密码，算法单次md5
-    :param apssword: 传入的密码
-    :return: 加密后的密码
-    """
-    md5 = hashlib.md5()
-    md5.update(password.encode())
-    password = md5.hexdigest()
-    return str(password)
+# constants
+Location = {'buyer': '/buyer_main/'}
 
 
+# login function
 def login(request):
-    ctx = {}
+    ctx = {}  # context
     if request.method == 'POST' and request.POST:
+        # get content from request
         usertype = request.POST.get("usertype")
         username = request.POST.get("username")
         password = request.POST.get("password")
-        user = DBUser.objects.get(usertype=usertype, username=username)
-        if user:  # username == "18307100100"
-            # now_password = setPassword(password)
+        # define
+        try:
+            user = DBUser.objects.get(usertype=usertype, username=username)
+        except DBUser.DoesNotExist:
+            user = None
+        if user:
             db_password = user.password
             if password == db_password:  # 密码正确
-                response = HttpResponseRedirect('/buyer_main/')  # 跳转至新的页面
-                response.set_cookie("username", username)  #
+                response = HttpResponseRedirect(Location[usertype])  # 跳转至新的页面
+                response.set_cookie("username", username)  # 设置cookie
                 return response
-            else:  # 密码错误
-                ctx['rlt'] = "用户或密码错误！"
+            else:  # 密码错误显示提醒
+                ctx['rlt'] = "密码错误！"
+        else:  # 用户名或类别显示提醒
+            ctx['rlt'] = "用户名或类别错误！"
 
     return render(request, "login.html", ctx)
 
