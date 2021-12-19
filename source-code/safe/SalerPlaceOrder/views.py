2985ea7d0fc3317b462fa1f74226bf1a3e0ae92d
qczhang0830 <93993915+qczhang0830@users.noreply.github.com>
2021-12-19 19:09:42 +0800

    Update views.py

--- source-code/safe/SalerPlaceOrder/views.py
+++ source-code/safe/SalerPlaceOrder/views.py
@@ -1,29 +1,31 @@
 # -*- coding: utf-8 -*-
 
-from django.shortcuts import render
-from SalerPlaceOrder.models import Saler
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from .models import SalerInfo, Company
 
 def placeorder(request):
-    request.encoding = 'utf-8'
-    """
-    在卖方订单表增加数据
-    """
-    result = ""
-    # 用于返回是否输入成功
-    if request.method == 'POST' and request.POST:
-        #判断提交方式是否为POST及POST是否有值
-        salerid = request.POST.get('salerid')
-        #获取前端输入的卖方id
-        orderid = request.POST.get('orderid')
-        # 获取前端输入的物流订单id
-        company = request.POST.get('company')
-        # 获取前端选择的物流公司
-        models.SaleInfo.objects.create(salerid=salerid, orderid=orderid, company=company)
+    try:
+        selected_company = Company.companyname.get(pk=request.POST['company'])
+    except (KeyError, Company.DoesNotExist):
+        # 未提交公司选项，重新展示表单
+        return render(request, 'SalerPlaceOrder/placeorder.html', {
+            'error_message': "You didn't select a company.",
+        })
+    else:
         """
-        SalerPlaceOrder.salerid = salerid
-        SalerPlaceOrder.orderid = orderid
-        SalerPlaceOrder.company = company
-        SalerPlaceOrder.save()
+           将数据写入库
         """
-        result = 'success'
-    return render(request, "salerhome.html", {'result':result})
+        salerid = request.POST.get('salerid')  # 获取前端输入的卖方id
+        orderid = request.POST.get('orderid')  # 获取前端输入的物流订单id
+        company = request.POST.get('company')  # 获取前端选择的物流公司
+        """
+           跳转链接（卖家主页），不写死
+           给出我们想要跳转的视图的名字和该视图所对应的URL模式中需要给该视图提供的参数
+        """
+        return HttpResponseRedirect(reverse('results', args=(salerid,)))
+
+def result(request, orderid):
+    salerid = get_object_or_404(SalerInfo, pk=orderid)
+    return render(request, 'SalerPlaceorder/salerhome.html', {'salerid': salerid})
