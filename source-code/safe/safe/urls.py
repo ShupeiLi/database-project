7845f26ebfc37aea8f883103b14444381519fda9
qczhang0830 <93993915+qczhang0830@users.noreply.github.com>
2021-12-19 19:05:13 +0800

    Update urls.py

--- source-code/safe/safe/urls.py
+++ source-code/safe/safe/urls.py
@@ -17,5 +17,6 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
+    path('placeorder/', include('SalerPlaceOrder.urls')),
     path('admin/', admin.site.urls),
 ]
