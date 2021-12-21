2da8e38d3356b1a634493b2f82614b03ec20bf1b
Dingyx0100 <dingyx_christina@outlook.com>
2021-12-21 15:50:51 +0800

    edit attributes

--- source-code/safe/dashboard/models.py
+++ source-code/safe/dashboard/models.py
@@ -1,13 +1,14 @@
 from django.db import models
+from register.models import NewUser
 
 
 # Create your models here.
 class OrderInformation(models.Model):
     ono = models.IntegerField(primary_key=True, unique=True, null=False)
     otime = models.DateField(null=False)
-    ovalue = models.FloatField(max_length=8, null=False)
-    username = models.CharField(max_length=64, null=False)  # Uno
-    sellername = models.CharField(max_length=64, null=False)  # Sno
+    ovalue = models.DecimalField(max_digit=8, decimal_places=2, null=False)
+    username = models.ForeignKey(to=NewUser,related_name="username")  # Uno
+    sellername = models.ForeignKey(to=NewUser,related_name="username")  # Sno
     otype = models.CharField(max_length=64, null=False)
     onum = models.IntegerField(max_length=4, null=False)
 
