b3bc249529b1f2d6c96e38a59519f7c6d0315729
ShupeiLi <shupei.tina.li@gmail.com>
2021-12-21 16:53:51 +0800

    Fix bugs

--- source-code/safe/dashboard/models.py
+++ source-code/safe/dashboard/models.py
@@ -1,16 +1,15 @@
 from django.db import models
-from register.models import NewUser
 
 
 # Create your models here.
 class OrderInformation(models.Model):
     ono = models.IntegerField(primary_key=True, unique=True, null=False)
     otime = models.DateField(null=False)
-    ovalue = models.DecimalField(max_digit=8, decimal_places=2, null=False)
-    username = models.ForeignKey(to=NewUser,related_name="username")  # Uno
-    sellername = models.ForeignKey(to=NewUser,related_name="username")  # Sno
+    ovalue = models.DecimalField(max_digits=8, decimal_places=2, null=False)
+    username = models.ForeignKey('register.NewUser', related_name='usernames', on_delete=models.CASCADE)  # Uno
+    sellername = models.ForeignKey('register.NewUser', related_name='sellernames', on_delete=models.CASCADE)  # Sno
     otype = models.CharField(max_length=64, null=False)
-    onum = models.IntegerField(max_length=4, null=False)
+    onum = models.IntegerField(null=False)
 
     def __str__(self):
         return self.ono
