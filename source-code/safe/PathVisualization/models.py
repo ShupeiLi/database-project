16743d6fea6b4bd5956b300d2487f2b00011de4b
qczhang0830 <93993915+qczhang0830@users.noreply.github.com>
2021-12-22 15:32:53 +0800

    Update models.py

--- source-code/safe/PathVisualization/models.py
+++ source-code/safe/PathVisualization/models.py
@@ -5,3 +5,9 @@ class Logistics_Location(models.Model):
     dno = models.CharField(max_length=200)
     dloc = models.CharField(max_length=200)
     dupdate = models.DateTimeField(max_length=200)
+    
+
+class covid19(models.Model):
+    date = models.DateTimeField(max_length=200)
+    place = models.CharField(max_length=200)
+    number = models.DateTimeField(max_length=200)
